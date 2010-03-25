458a17d0a5e5708cababa9c94c84c9f32e9521f9
Christian Köstlin <christian.koestlin@gmail.com>
2010-03-25 01:19:41 +0100

    more round ... first and last class for very first and very last of the navigation divs

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,4 +1,4 @@
-def print_parents(item, items, first, last)
+def get_parents(item, items)
   all = []
   while item[:parent] != nil
     item = items.find{|i| i.identifier == item[:parent]}
@@ -7,20 +7,20 @@ def print_parents(item, items, first, last)
     end
     all << item
   end
-  do_collection(all.reverse, 'parent', first, last) do |i, j|
-    yield(i,j)
-  end
+  return all.reverse
 end
 
 def do_collection(collection, context, first, last)
   collection.each_with_index do |i, idx|
     help = context.dup
-    if idx == 0
+    is_first = (idx == 0)
+    is_last = (idx == (collection.size-1))
+    if is_first
       help = "#{help}#{first}"
     end
-    if idx == (collection.size-1)
+    if is_last
       help = "#{help}#{last}"
     end
-    yield(i, help)
+    yield(i, help, is_first, is_last )
   end
 end
