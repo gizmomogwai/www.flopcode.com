3f70cd551b9611b659cc3b7cf9ced767fe972e3e
Christian Köstlin <christian.koestlin@gmail.com>
2010-04-14 01:11:33 +0200

    font experimente

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -24,3 +24,7 @@ def do_collection(collection, context, first, last)
     yield(i, help, is_first, is_last )
   end
 end
+
+def lorem
+  "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+end
